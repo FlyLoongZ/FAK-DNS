89b984b11f2f9713f83e98eadeffe7ce41406c45
FlyLoongZ <60963681+FlyLoongZ@users.noreply.github.com>
2025-11-14 02:52:23 +0800

    feat: 分流文件提交到file分支

--- convert.py
+++ convert.py
@@ -16,18 +16,16 @@ def convert_conf_to_txt(conf_file, txt_file, cn_dns):
 
 def main():
     current_directory = os.getcwd()  # 获取当前目录
-    converted_directory = os.path.join(current_directory, 'converted')  # 创建 converted 文件夹
-    os.makedirs(converted_directory, exist_ok=True)  # 确保 converted 文件夹存在
     # 使用 glob 匹配以 .conf 结尾的文件
     conf_files = glob.glob(os.path.join(current_directory, '*china.conf'))
     # 逐个读取文件内容
     for thefile in conf_files:
-        txt_file = os.path.join(converted_directory, os.path.basename(thefile) + ".txt")  # 生成的 txt 文件路径
+        txt_file = os.path.join(current_directory, os.path.basename(thefile) + ".txt")  # 生成的 txt 文件路径
         convert_conf_to_txt(thefile, txt_file, cn_dns)
 
     # 合并生成的 txt 文件为 FAK-DNS.txt
-    txt_files = glob.glob(os.path.join(converted_directory, '*conf.txt'))
-    with open(os.path.join(converted_directory, 'FAK-DNS.txt'), 'w') as fak_dns:
+    txt_files = glob.glob(os.path.join(current_directory, '*conf.txt'))
+    with open(os.path.join(current_directory, 'FAK-DNS.txt'), 'w') as fak_dns:
         fak_dns.write(f"# 文件生成时间: {current_time}\n")
         fak_dns.write(the_dns + "\n")  # 新增行，内容为自定义内容
         for txt_file in txt_files:
@@ -35,7 +33,7 @@ def main():
                 fak_dns.write(txt.read())
 
     # 添加 the_dns 到其他配置文件
-    for file_path in glob.glob('converted/*.conf.txt'):
+    for file_path in glob.glob('*.conf.txt'):
         with open(file_path, 'r') as file:
             existing_content = file.read()
         with open(file_path, 'w') as file:
